4855a4139f71b76378305144832f64d8e2b6ecab
Benie <davinci.monalissa@gmail.com>
2022-03-07 21:42:26 +0300

    Add variable definitions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 const name = document.querySelector(".name");
 function akanName() {
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var myDob = document.getElementById("#Day");
+  var myYob = document.getElementById("#Year");
+  var myGender = document.getElementById("#Gender");
+  var akanName = maleName[finDay];
 
-  
+
+
 }
